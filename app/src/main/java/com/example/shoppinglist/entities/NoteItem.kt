6389ce267460b49b561fb5ef251aee3019a642e1
ronin <https://github.com/roninkuma/AlyonaNail>
2022-01-27 05:19:48 +0900

    new2

--- app/src/main/java/com/example/shoppinglist/entities/NoteItem.kt
+++ app/src/main/java/com/example/shoppinglist/entities/NoteItem.kt
@@ -1,5 +1,6 @@
 package com.example.shoppinglist.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,4 +8,16 @@ import androidx.room.PrimaryKey
 data class NoteItem(
     @PrimaryKey(autoGenerate = true)
     val id: Int?,
+
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "content")
+    val content: String,
+
+    @ColumnInfo(name = "time")
+    val time: String,
+
+    @ColumnInfo(name ="category")
+    val category: String,
 )
