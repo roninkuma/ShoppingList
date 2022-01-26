6aa779550ae365f55611cf0b5b791e0bdefafcfd
ronin <https://github.com/roninkuma/AlyonaNail>
2022-01-27 05:22:36 +0900

    new2

--- app/src/main/java/com/example/shoppinglist/entities/LibraryItem.kt
+++ app/src/main/java/com/example/shoppinglist/entities/LibraryItem.kt
@@ -1,3 +1,15 @@
 package com.example.shoppinglist.entities
 
-data class LibraryItem()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "library")
+data class LibraryItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo(name = "name")
+    val name: String
+
+)
