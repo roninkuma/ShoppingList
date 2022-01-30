026054ee1e01c073777710be40c2653e0b4c7f12
ronin <roninkuma.in.it@gmail.com>
2022-01-31 01:39:47 +0900

    newLog

--- app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
@@ -2,11 +2,42 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ActivityMainBinding
 
  class MainActivity : AppCompatActivity() {
+
+     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavListener()
     }
+
+     private fun setBottomNavListener(){
+         binding.bNav.setOnItemSelectedListener {
+             when(it.itemId){
+                 R.id.settings->{
+                     Log.d("MyLog","Setting")
+
+                 }
+                 R.id.notes->{
+                     Log.d("MyLog","Notes")
+
+                 }
+                 R.id.shop_list->{
+                     Log.d("MyLog","List")
+
+                 }
+                 R.id.new_item->{
+                     Log.d("MyLog","New")
+
+                 }
+             }
+             true
+         }
+     }
 }
